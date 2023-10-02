c54c4902bf94e0018d5bf64b3191276b3b0f36a8
Mátyás Pintér <p.maty01@gmail.com>
2023-10-02 17:20:07 +0200

    Modified log path to work universally

--- raspberry-pi/logger.py
+++ raspberry-pi/logger.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 logger = logging.getLogger()
 logger.setLevel(logging.getLevelName('INFO'))
@@ -6,7 +7,7 @@ log_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(messa
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(log_formatter)
 logger.addHandler(console_handler)
-fh = logging.FileHandler('logs/output.log', mode='w')
+fh = logging.FileHandler(os.path.dirname(os.path.abspath(__file__)) + '/logs/output.log', mode='w')
 fh.setLevel(logging.getLevelName('DEBUG'))
 fh.setFormatter(log_formatter)
 logger.addHandler(fh)
