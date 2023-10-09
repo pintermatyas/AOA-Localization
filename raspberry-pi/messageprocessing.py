19f368f272cd2e3c9ae0c797710550dd3c807646
Mátyás Pintér <p.maty01@gmail.com>
2023-10-09 10:48:49 +0200

    Raspberry Pi now sends the bounding box of anchor positions alongside the calculated position

--- raspberry-pi/messageprocessing.py
+++ raspberry-pi/messageprocessing.py
@@ -1,18 +1,26 @@
 import logger
-import threading
 import positioning
-import processing
 import constants
+import numpy as np
 import pandas as pd
 from egress_udp_server import multicast_udp_message
 
 logger = logger.get_logger()
-anchors = pd.read_csv(constants.CONFIG_FOLDER + '/anchors.csv')
-anchors = anchors.set_index(anchors['anchor_id'])
 
 def process_message(message):
+    anchors = pd.read_csv(constants.CONFIG_FOLDER + '/anchors.csv')
+    anchors = anchors.set_index(anchors['anchor_id'])
     logger.info("Processing message " + str(message))
     df = pd.DataFrame.from_dict(message)
     bps = positioning.BluetoothPositionSystem()
     est_pos = bps.aoa_2(df, anchors)
-    multicast_udp_message(est_pos)
+    avg_pos = np.nanmean(est_pos['est_pos'], axis=0)
+    lowest_x = anchors['x'].min()
+    highest_x = anchors['x'].max()
+    lowest_y = anchors['y'].min()
+    highest_y = anchors['y'].max()
+
+    lowest_coordinates = (lowest_x, lowest_y)
+    highest_coordinates = (highest_x, highest_y)
+
+    multicast_udp_message(str(avg_pos) + ";" + str(lowest_coordinates) + "," + str(highest_coordinates))
