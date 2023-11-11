ec0332b6c6e1696afda21bce49719f1d1dd4e019
Mátyás Pintér <p.maty01@gmail.com>
2023-11-11 22:24:08 +0100

    Updated measurement mode
    Included in the feature:
     - Measurements for each tags are separated
     - Each measurement instance has its own folder
     - Configuration is copied into the measurement's folder (tag coordinates, anchor configuration file)
     - Added test measurements
     - Measurement folder is moved into the repository
    
    Additions:
     - Threads are now daemon threads so they don't block the process on exit

--- raspberry-pi/init.py
+++ raspberry-pi/init.py
@@ -1,6 +1,25 @@
 import ingress_udp_server
 import subscription_handler
+import constants
+import os
+import logger
 from threading import Thread
+logger = logger.get_logger()
 
-Thread(target=subscription_handler.start_subscription_handler).start()
-Thread(target=ingress_udp_server.start_ingress_udp_server).start()
+if not os.path.exists(constants.MEASUREMENT_FOLDER):
+    os.makedirs(constants.MEASUREMENT_FOLDER)
+    logger.info("Created measurements folder")
+else:
+    logger.info("Measurements folder already exists!")
+
+try:
+    subscription = Thread(target=subscription_handler.start_subscription_handler, daemon=True)
+    ingress_server = Thread(target=ingress_udp_server.start_ingress_udp_server, daemon=True)
+    subscription.start()
+    ingress_server.start()
+    subscription.join()
+    ingress_server.join()
+except KeyboardInterrupt:
+    if constants.COLLECT_MEASUREMENTS:
+        logger.error("Got KeyboardInterrupt. Saving measurements on exit.")
+        ingress_udp_server.save_on_exit()
--- raspberry-pi/test_positioning.py
+++ raspberry-pi/test_positioning.py
@@ -44,9 +44,12 @@ def send_spoofed_packet(src_ip, dst_ip, src_port, dst_port, payload):
     send(packet)
 
 def test_real_life_scenario():
-    first = "\r\n+UUDF:6C1DEBA4241B,-65,26,-65,6,37,\"CCF9579B21AE\",\"\",1129320\r\n"
+    first = "\r\n+UUDF:6C1DEBA4241B,-65,26,-65,6,37,\"CCF9579B2279\",\"\",1129320\r\n"
     second = "\r\n+UUDF:6C1DEBA4241B,-59,32,-67,0,39,\"CCF9579B217F\",\"\",1117130\r\n"
-    third = "\r\n+UUDF:6C1DEBA4241B,-62,38,-58,8,39,\"CCF9579B22B0\",\"\",1111242\r\n"
+    third = "\r\n+UUDF:6C1DEBA4241B,-62,38,-58,8,39,\"CCF9579B223C\",\"\",1111242\r\n"
+    fourth = "\r\n+UUDF:6C1DEBA4241C,-65,26,-65,6,37,\"CCF9579B2279\",\"\",1129320\r\n"
+    fifth = "\r\n+UUDF:6C1DEBA4241C,-59,32,-67,0,39,\"CCF9579B217F\",\"\",1117130\r\n"
+    sixth = "\r\n+UUDF:6C1DEBA4241C,-62,38,-58,8,39,\"CCF9579B223C\",\"\",1111242\r\n"
 
     server_address = (constants.GATEWAY_IP, constants.INGRESS_SERVER_PORT)
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -57,6 +60,12 @@ def test_real_life_scenario():
         send_spoofed_packet("1.2.3.5", constants.GATEWAY_IP, 12345, constants.INGRESS_SERVER_PORT, second)
         #udp_socket.sendto(third.encode('utf-8'), server_address)
         send_spoofed_packet("1.2.3.6", constants.GATEWAY_IP, 12345, constants.INGRESS_SERVER_PORT, third)
+        #udp_socket.sendto(first.encode('utf-8'), server_address)
+        send_spoofed_packet("1.2.3.4", constants.GATEWAY_IP, 12345, constants.INGRESS_SERVER_PORT, fourth)
+        #udp_socket.sendto(second.encode('utf-8'), server_address)
+        send_spoofed_packet("1.2.3.5", constants.GATEWAY_IP, 12345, constants.INGRESS_SERVER_PORT, fifth)
+        #udp_socket.sendto(third.encode('utf-8'), server_address)
+        send_spoofed_packet("1.2.3.6", constants.GATEWAY_IP, 12345, constants.INGRESS_SERVER_PORT, sixth)
         print("sent to " + str(server_address))
 
 test_real_life_scenario()
